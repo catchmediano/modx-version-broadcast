8acbb2c00e6bb1bb62e52adf1ebd700e53c1a52c
OptimusCrime <thomasgautv@hotmail.com>
2017-09-18 00:06:19 +0200

    Add 1.0.0 package

--- _build/build.config.php
+++ _build/build.config.php
@@ -1,5 +1,5 @@
 <?php
 define('PKG_NAME', 'VersionBroadcast');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
-define('PKG_VERSION', '0.1.0');
+define('PKG_VERSION', '1.0.0');
 define('PKG_RELEASE', 'pl');
